0bb3886f73544b329bcdb84a084c6721bda6e134
Slava_Klepikov <SlavaKlepikov@bigmir.net>
2018-10-27 21:31:55 +0300

    added Allure

--- src/main/java/pojo/issue/updatepriority/Update.java
+++ src/main/java/pojo/issue/updatepriority/Update.java
@@ -5,7 +5,7 @@ import pojo.Pojo;
 import java.util.ArrayList;
 
 public class Update implements Pojo {
-    public ArrayList<Object> priority =new ArrayList<>();
+    public ArrayList<Object> priority =new ArrayList<Object>();
 
     public Update setPriority(Object set) {
         this.priority.add(set);
